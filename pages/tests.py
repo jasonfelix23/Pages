22f6413da929ba8c930ffd0a973ba2864564b90c
Jason Felix <jasonfelix789@gmail.com>
2021-05-04 20:07:59 +0530

    Pipfile and Pipfile.lock

--- pages/tests.py
+++ pages/tests.py
@@ -8,5 +8,5 @@ class SimpleTests(SimpleTestCase):
 
 
     def test_abut_page_status_code(self):
-        response = self.client.get('/')
+        response = self.client.get('/about')
         self.assertEqual(response.status_code, 200)
